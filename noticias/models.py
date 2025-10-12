afe0cc25d7c96a9015bc8089040c561c8ceba56f
Bruno Augusto <bruno.filho1000@gmail.com>
2025-10-12 20:50:17 -0300

    Update models.py

--- noticias/models.py
+++ noticias/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
 from django.utils import timezone
+from django.urls import reverse
 
 class Assunto(models.Model):
     nome = models.CharField(max_length=80, unique=True)
@@ -33,6 +34,8 @@ class Noticia(models.Model):
         related_name='noticias_salvas',
         blank=True,
     )
+    def get_absolute_url(self):
+        return reverse("noticias:noticia_detalhe", args=[self.pk])
 
     @property
     def votos(self):
