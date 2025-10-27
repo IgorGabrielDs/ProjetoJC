4d3a585e526882eba13128d728701fb10ffff2c5
Bruno Augusto <bruno.filho1000@gmail.com>
2025-10-27 11:34:55 -0300

    Create backends.py

---
+++ noticias/backends.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
+from django.db.models import Q
+
+class EmailOrUsernameModelBackend(ModelBackend):
+    """Autentica usando e-mail OU username no mesmo campo 'username'."""
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        User = get_user_model()
+        if username is None:
+            username = kwargs.get(User.USERNAME_FIELD)
+
+        if not username or not password:
+            return None
+
+        user = (
+            User.objects
+            .filter(Q(email__iexact=username) | Q(**{User.USERNAME_FIELD: username}))
+            .order_by("id")
+            .first()
+        )
+        if not user:
+            return None
+
+        if user.check_password(password) and self.user_can_authenticate(user):
+            return user
+        return None
