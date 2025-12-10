ada6b6066d34815c2864fdfe6f97fea56c431f25
Gustavotorrescastro <gustavotorrescastro@gmail.com>
2025-12-10 20:17:43 -0300

    Últimas atualizações

--- JornalDoCommercio/settings.py
+++ JornalDoCommercio/settings.py
@@ -84,10 +84,19 @@ WSGI_APPLICATION = "JornalDoCommercio.wsgi.application"
 # ---------------------------------------------------------
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DBNAME"),
+        "USER": os.getenv("DBUSER"),
+        "PASSWORD": os.getenv("DBPASSWORD"),
+        "HOST": os.getenv("DBHOST"),
+        "PORT": os.getenv("DBPORT"),
+        "OPTIONS": {
+            "sslmode": os.getenv("DBSSLMODE"),
+        },
     }
 }
+
+
 
 # ---------------------------------------------------------
 # SENHAS
