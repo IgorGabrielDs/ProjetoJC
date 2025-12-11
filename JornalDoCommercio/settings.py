28e239b31fd520f92bb72f52eef1b7c066be4c0d
Gustavotorrescastro <gustavotorrescastro@gmail.com>
2025-12-10 21:32:54 -0300

    Últimas atualizações

--- JornalDoCommercio/settings.py
+++ JornalDoCommercio/settings.py
@@ -1,87 +1,76 @@
 from pathlib import Path
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(os.path.join(BASE_DIR, ".env"))
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-# DEBUG automatico: somente local
 DEBUG = os.getenv("WEBSITE_HOSTNAME") is None
 
-ALLOWED_HOSTS = [
-    "jcproject.azurewebsites.net",
-    "127.0.0.1",
-    "localhost",
+ALLOWED_HOSTS = ['jcproject.azurewebsites.net', '127.0.0.1', 'localhost']
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    "caca_links",
+    'django_apscheduler',
+    "sudoku",
+    'noticias.apps.NoticiasConfig',
+    'storage',
 ]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'JornalDoCommercio.urls'
 
 CSRF_TRUSTED_ORIGINS = [
     "https://jcproject.azurewebsites.net",
 ]
 
-# ---------------------------------------------------------
-# APPS
-# ---------------------------------------------------------
-INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-
-    # libs extras
-    "django_apscheduler",
-    "storages",  # <--- NECESSÁRIO PARA AZURE
-
-    # seus apps
-    "caca_links",
-    "sudoku",
-    "noticias.apps.NoticiasConfig",
-]
-
-# ---------------------------------------------------------
-# MIDDLEWARE
-# ---------------------------------------------------------
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-
-    # whitenoise serve ARQUIVOS ESTÁTICOS
-    "whitenoise.middleware.WhiteNoiseMiddleware", 
-
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
-
-ROOT_URLCONF = "JornalDoCommercio.urls"
-
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = "JornalDoCommercio.wsgi.application"
+WSGI_APPLICATION = 'JornalDoCommercio.wsgi.application'
 
-# ---------------------------------------------------------
-# BANCO
-# Pode continuar usando SQLite, mas não é persistente.
-# ---------------------------------------------------------
+APSCHEDULER_DATETIME_FORMAT = "N j, Y, f:s a"
+
+APSCHEDULER_RUN_MIGRATIONS = True
+
+
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -94,53 +83,52 @@ DATABASES = {
             "sslmode": os.getenv("DBSSLMODE"),
         },
     }
-}
+} 
+        
 
-
-
-# ---------------------------------------------------------
-# SENHAS
-# ---------------------------------------------------------
 AUTH_PASSWORD_VALIDATORS = [
-    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
 ]
 
-LANGUAGE_CODE = "pt-br"
-TIME_ZONE = "America/Recife"
+
+LANGUAGE_CODE = 'pt-br'
+
+TIME_ZONE = 'America/Recife'
+
 USE_I18N = True
+
 USE_TZ = True
 
-# ---------------------------------------------------------
-# STATIC (funciona 100% na Azure com Whitenoise)
-# ---------------------------------------------------------
+
 STATIC_URL = "/static/"
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
     BASE_DIR / "noticias" / "static",
 ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# ---------------------------------------------------------
-# MEDIA (USANDO AZURE BLOB STORAGE)
-# ---------------------------------------------------------
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# ESSA LINHA É A MUDANÇA CRÍTICA
-DEFAULT_FILE_STORAGE = "storages.backends.azure_storage.AzureStorage"
 
-AZURE_ACCOUNT_NAME = os.getenv("AZURE_ACCOUNT_NAME")
-AZURE_ACCOUNT_KEY = os.getenv("AZURE_ACCOUNT_KEY")
-AZURE_CONTAINER = "media"  # crie no Azure Storage
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
-MEDIA_URL = f"https://{AZURE_ACCOUNT_NAME}.blob.core.windows.net/{AZURE_CONTAINER}/"
+os.makedirs(MEDIA_ROOT, exist_ok=True)
 
-# ---------------------------------------------------------
-# AUTENTICAÇÃO
-# ---------------------------------------------------------
 AUTHENTICATION_BACKENDS = [
     "noticias.backends.EmailOrUsernameModelBackend",
     "django.contrib.auth.backends.ModelBackend",
@@ -149,5 +137,3 @@ AUTHENTICATION_BACKENDS = [
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
-
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
